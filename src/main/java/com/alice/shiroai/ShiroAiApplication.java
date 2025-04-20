829b3d0e30fed08c828b692c0998e99a4af20f08
code-with-Anson <2795757650@qq.com>
2025-04-21 01:48:07 +0800

    初始化项目，引入spring ai

---
+++ src/main/java/com/alice/shiroai/ShiroAiApplication.java
@@ -0,0 +1,13 @@
+package com.alice.shiroai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alice/shiroai/ShiroAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alice.shiroai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
