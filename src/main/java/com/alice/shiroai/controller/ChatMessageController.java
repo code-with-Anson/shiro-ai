9bc845541076dc5004acd0d1d672d2e4876e3f3c
code-with-Anson <2795757650@qq.com>
2025-04-24 17:41:22 +0800

    合并不必要的类，修改规范

--- src/main/java/com/alice/shiroai/controller/ChatMessageController.java
+++ src/main/java/com/alice/shiroai/controller/ChatMessageController.java
@@ -1,20 +1,32 @@
 package com.alice.shiroai.controller;
 
-
+import com.alice.shiroai.service.impl.ChatMessageServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-/**
- * <p>
- * 聊天消息记录表 前端控制器
- * </p>
- *
- * @author Anson
- * @since 2025-04-23
- */
 @RestController
-@RequestMapping("/chat-message")
+@RequestMapping("/ai")
+@Tag(name = "ai聊天控制器")
+@Slf4j
 public class ChatMessageController {
+    private final ChatMessageServiceImpl chatMessageServiceImpl;
 
+    public ChatMessageController(ChatMessageServiceImpl chatMessageServiceImpl) {
+        this.chatMessageServiceImpl = chatMessageServiceImpl;
+    }
+
+    @GetMapping(value = "/flux-ChatClient/OpenAi-momoi", produces = "text/html;charset=UTF-8")
+    @Operation(summary = "链式实现ChatClient聊天接口")
+    public Flux<String> chatWithOpenAiByMomoi(@RequestParam(value = "message", defaultValue = "给我讲个笑话？") String message) {
+        log.info("链式构成ChatClient/OpenAi聊天接口被调用，消息内容：{}", message);
+        Flux<String> fluxchat = chatMessageServiceImpl.chatWithOpenAiByMomoi(message);
+        log.info("{}调用完成", "momoi");
+        return fluxchat;
+    }
 }
--- src/main/java/com/alice/shiroai/service/impl/ChatMessageServiceImpl.java
+++ src/main/java/com/alice/shiroai/service/impl/ChatMessageServiceImpl.java
@@ -1,20 +1,38 @@
 package com.alice.shiroai.service.impl;
 
-import com.alice.shiroai.domain.po.ChatMessage;
-import com.alice.shiroai.mapper.ChatMessageMapper;
-import com.alice.shiroai.service.IChatMessageService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
-/**
- * <p>
- * 聊天消息记录表 服务实现类
- * </p>
- *
- * @author Anson
- * @since 2025-04-23
- */
+
 @Service
-public class ChatMessageServiceImpl extends ServiceImpl<ChatMessageMapper, ChatMessage> implements IChatMessageService {
+public class ChatMessageServiceImpl {
+    private final ChatClient Momoi;
+
+    @Value("classpath:/prompts/alice-system-message.st")
+    private Resource systemResource;
+
+    public ChatMessageServiceImpl(ChatClient Momoi) {
+        this.Momoi = Momoi;
+    }
+
+    /**
+     * 使用Momoi进行聊天（直接使用链式编程传入参数）
+     *
+     * @param message
+     * @return
+     */
+    public Flux<String> chatWithOpenAiByMomoi(String message) {
+
+        // 直接使用链式编程传入参数
+        Flux<String> midori = Momoi.prompt()
+                .user(message)
+                .system(systemResource)
+                .stream()
+                .content();
+        return midori;
+    }
 
 }
